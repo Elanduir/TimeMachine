42f23c395ff8106dedd5e29a4f9a3399507a4cba
Fabian Heuberger <elanduir@gmail.com>
2022-09-17 16:00:54 +0200

    first draft of timemachine interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,22 +10,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Time Machine',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,84 +24,243 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  DateTime date = DateTime.now();
+  DateTime targetDate = DateTime.now();
+  bool inProgress = false;
+  Duration increment = const Duration(hours: 3);
+  TextStyle tStyle = const TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
 
-  void _incrementCounter() {
+  TextStyle cStyle = const TextStyle(
+    fontSize: 80,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  }
+
+  void _startCountdown() async{
+    inProgress = !inProgress;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      inProgress;
+    });
+    String mode = "";
+    mode = targetDate.compareTo(date) < 0 ? "-" : "+";
+    if(mode == "-"){
+      while(targetDate.compareTo(date) < 0){
+        _countDown(mode);
+        await Future.delayed(const Duration(milliseconds: 2));
+      }
+      date = date.add(increment);
+    }else{
+      while(targetDate.compareTo(date) > 0){
+        _countDown(mode);
+        await Future.delayed(const Duration(milliseconds: 2));
+      }
+      date = date.subtract(increment);
+    }
+  }
+
+  void _modDay(String mode, Duration dur){
+    if(dur == const Duration(days: 99)){
+      if(mode == '-'){
+        targetDate = DateTime(targetDate.year, targetDate.month - 1, targetDate.day, targetDate.hour, targetDate.minute);
+      }else{
+        targetDate = DateTime(targetDate.year, targetDate.month + 1, targetDate.day, targetDate.hour, targetDate.minute);
+      }
+    }else if(dur == const Duration(days: 365)){
+      if(mode == '-'){
+        targetDate = DateTime(targetDate.year - 1, targetDate.month, targetDate.day, targetDate.hour, targetDate.minute);
+      }else{
+        targetDate = DateTime(targetDate.year + 1, targetDate.month, targetDate.day, targetDate.hour, targetDate.minute);
+      }
+    }else{
+      if(mode == '-'){
+        targetDate = targetDate.subtract(dur);
+      }else{
+        targetDate = targetDate.add(dur);
+      }
+    }
+    setState(() {
+      targetDate;
+    });
+  }
+
+  void _countDown(String mode) {
+    if(mode == "-"){
+      date = date.subtract(increment);
+    }else{
+      date = date.add(increment);
+    }
+    
+    setState(() {
+      date;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: null,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Visibility(
+              visible: !inProgress,
+              child: Wrap(
+              spacing: 20,
+              children: [
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _modDay("+", const Duration(days: 1)),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                    Text(
+                      DateFormat('dd').format(date),
+                      style: tStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _modDay("-", const Duration(days: 1)),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _modDay("+", const Duration(days: 99)),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                    Text(
+                      DateFormat('MM').format(date),
+                      style: tStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _modDay("-", const Duration(days: 99)),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _modDay("+", const Duration(days: 365)),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                    Text(
+                      DateFormat('yyyy').format(date),
+                      style: tStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _modDay("-", const Duration(days: 365)),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _modDay("+", const Duration(hours: 1)),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                    Text(
+                      DateFormat('HH').format(date),
+                      style: tStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _modDay("-", const Duration(hours: 1)),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _modDay("+", const Duration(minutes: 1)),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                    Text(
+                      DateFormat('mm').format(date),
+                      style: tStyle,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _modDay("-", const Duration(minutes: 1)),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  ],
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ),
+            Visibility(
+                visible: inProgress,
+                child: Wrap(
+              spacing: 20,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      DateFormat('dd').format(date),
+                      style: cStyle,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      DateFormat('MM').format(date),
+                      style: cStyle,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      DateFormat('yyyy').format(date),
+                      style: cStyle,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      DateFormat('HH').format(date),
+                      style: cStyle,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      DateFormat('mm').format(date),
+                      style: cStyle,
+                    ),
+                  ],
+                )
+              ],
+            ),
             ),
           ],
-        ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _startCountdown,
+        tooltip: 'Start',
+        child: const Icon(Icons.play_arrow),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
