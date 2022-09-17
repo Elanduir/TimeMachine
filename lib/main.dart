6869deb835e06460305b0a7298fa50967802a8b1
Fabian Heuberger <elanduir@gmail.com>
2022-09-17 21:29:07 +0200

    Changed interface to darkmode and changed font added support for sharedprefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'dart:developer';
 
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +14,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,88 +30,195 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class TimeStrings {
+  String day = "";
+  String month = "";
+  String year = "";
+  String hour = "";
+  String minute = "";
+
+  void update(DateTime date) {
+    day = DateFormat('dd').format(date);
+    month = DateFormat('MM').format(date);
+    year = DateFormat('yyyy').format(date);
+    hour = DateFormat('HH').format(date);
+    minute = DateFormat('mm').format(date);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   DateTime date = DateTime.now();
   DateTime targetDate = DateTime.now();
+  TimeStrings dateString = TimeStrings();
+  TimeStrings targetString = TimeStrings();
   bool inProgress = false;
-  Duration increment = const Duration(hours: 3);
-  TextStyle tStyle = const TextStyle(
-    fontSize: 60,
-    fontWeight: FontWeight.bold,
-  );
+  Duration increment = const Duration(days: 5);
+  String travelText = "";
+  Color defaultGreen = const Color(0xFF20c20e);
 
-  TextStyle cStyle = const TextStyle(
-    fontSize: 80,
-    fontWeight: FontWeight.bold,
+  TextStyle subTStyle = const TextStyle();
+  TextStyle tStyle = const TextStyle();
+
+  TextStyle subCStyle = const TextStyle();
+  TextStyle cStyle = const TextStyle();
+
+  late AudioPlayer player;
+
+  ButtonStyle bStyle = const ButtonStyle();
+
+  BoxDecoration decFin = const BoxDecoration(
+      color: Colors.black
   );
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+
+    String cacheDate = "";
+    SharedPreferences.getInstance().then((SharedPreferences prefVal) => {
+      cacheDate = prefVal.getString("current") ?? "",
+      setState((){
+        date = DateTime.parse(cacheDate);
+        dateString.update(date);
+        targetDate = DateTime.parse(cacheDate);
+        targetString.update(targetDate);
+      })
+    });
+
+
+    dateString.update(date);
+    targetString.update(targetDate);
+    player = AudioPlayer();
+
+    subTStyle =  TextStyle(
+      fontSize: 60,
+      fontWeight: FontWeight.bold,
+      color: defaultGreen,
+    );
+
+    subCStyle = TextStyle(
+      fontSize: 80,
+      fontWeight: FontWeight.bold,
+      color: defaultGreen,
+    );
+
+    tStyle = GoogleFonts.chakraPetch(
+      textStyle: subTStyle,
+    );
+
+    cStyle = GoogleFonts.chakraPetch(
+      textStyle: subCStyle,
+    );
+
+    bStyle = ButtonStyle(
+      foregroundColor: MaterialStateProperty.all<Color>(defaultGreen),
+      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+    );
   }
 
-  void _startCountdown() async{
+  void _startCountdown() async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setString("current", DateFormat('yyyy-dd-MM HH:mm:00').format(targetDate));
+
+    await player.setAsset('assets/TimeMachine.mp3');
+    await player.setLoopMode(LoopMode.one);
     inProgress = !inProgress;
     setState(() {
       inProgress;
     });
+
+    if(inProgress){
+      travelText = "Traveling...";
+      player.play();
+    }
+
     String mode = "";
     mode = targetDate.compareTo(date) < 0 ? "-" : "+";
-    if(mode == "-"){
-      while(targetDate.compareTo(date) < 0){
+    if (mode == "-") {
+      while (targetDate.compareTo(date) < 0) {
         _countDown(mode);
         await Future.delayed(const Duration(milliseconds: 2));
       }
-      date = date.add(increment);
-    }else{
-      while(targetDate.compareTo(date) > 0){
+    } else {
+      while (targetDate.compareTo(date) > 0) {
         _countDown(mode);
         await Future.delayed(const Duration(milliseconds: 2));
       }
-      date = date.subtract(increment);
+    }
+    player.stop();
+    date = targetDate;
+    dateString.update(date);
+    travelText = "Welcome to:";
+    if(inProgress){
+      for(int i = 0; i < 10; i++){
+        if(i%2 == 0){
+          decFin = BoxDecoration(
+              color: defaultGreen
+          );
+        }else{
+          decFin = const BoxDecoration(
+              color: Colors.black
+          );
+        }
+        setState(() {
+          decFin;
+        });
+        await Future.delayed(const Duration(milliseconds: 100));
+      }
     }
   }
 
-  void _modDay(String mode, Duration dur){
-    if(dur == const Duration(days: 99)){
-      if(mode == '-'){
-        targetDate = DateTime(targetDate.year, targetDate.month - 1, targetDate.day, targetDate.hour, targetDate.minute);
-      }else{
-        targetDate = DateTime(targetDate.year, targetDate.month + 1, targetDate.day, targetDate.hour, targetDate.minute);
+  void _modDate(String mode, Duration dur) {
+    if (dur == const Duration(days: 99)) {
+      if (mode == '-') {
+        targetDate = DateTime(targetDate.year, targetDate.month - 1,
+            targetDate.day, targetDate.hour, targetDate.minute);
+      } else {
+        targetDate = DateTime(targetDate.year, targetDate.month + 1,
+            targetDate.day, targetDate.hour, targetDate.minute);
       }
-    }else if(dur == const Duration(days: 365)){
-      if(mode == '-'){
-        targetDate = DateTime(targetDate.year - 1, targetDate.month, targetDate.day, targetDate.hour, targetDate.minute);
-      }else{
-        targetDate = DateTime(targetDate.year + 1, targetDate.month, targetDate.day, targetDate.hour, targetDate.minute);
+    } else if (dur == const Duration(days: 365)) {
+      if (mode == '-') {
+        targetDate = DateTime(targetDate.year - 1, targetDate.month,
+            targetDate.day, targetDate.hour, targetDate.minute);
+      } else {
+        targetDate = DateTime(targetDate.year + 1, targetDate.month,
+            targetDate.day, targetDate.hour, targetDate.minute);
       }
-    }else{
-      if(mode == '-'){
+    } else {
+      if (mode == '-') {
         targetDate = targetDate.subtract(dur);
-      }else{
+      } else {
         targetDate = targetDate.add(dur);
       }
     }
+    targetString.update(targetDate);
     setState(() {
       targetDate;
+      targetString;
     });
   }
 
   void _countDown(String mode) {
-    if(mode == "-"){
+    if (mode == "-") {
       date = date.subtract(increment);
-    }else{
+    } else {
       date = date.add(increment);
     }
-    
+    dateString.update(date);
+    //log('${dateString.day} ${dateString.month} ${dateString.year} ${dateString.hour} ${dateString.minute}');
     setState(() {
       date;
+      dateString;
     });
   }
 
@@ -114,153 +226,181 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Visibility(
-              visible: !inProgress,
-              child: Wrap(
-              spacing: 20,
-              children: [
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _modDay("+", const Duration(days: 1)),
-                      child: const Icon(Icons.arrow_upward),
+      body: Container(
+          decoration: decFin,
+          child:  Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Visibility(
+                    visible: !inProgress,
+                    child: Wrap(
+                      spacing: 20,
+                      children: [
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => _modDate("+", const Duration(days: 1)),
+                              child: const Icon(Icons.arrow_upward),
+                              style: bStyle,
+                            ),
+                            Text(
+                              targetString.day,
+                              style: tStyle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () => _modDate("-", const Duration(days: 1)),
+                              child: const Icon(Icons.arrow_downward),
+                              style: bStyle,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => _modDate("+", const Duration(days: 99)),
+                              child: const Icon(Icons.arrow_upward),
+                              style: bStyle,
+                            ),
+                            Text(
+                              targetString.month,
+                              style: tStyle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () => _modDate("-", const Duration(days: 99)),
+                              child: const Icon(Icons.arrow_downward),
+                              style: bStyle,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => _modDate("+", const Duration(days: 365)),
+                              child: const Icon(Icons.arrow_upward),
+                              style: bStyle,
+                            ),
+                            Text(
+                              targetString.year,
+                              style: tStyle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () => _modDate("-", const Duration(days: 365)),
+                              child: const Icon(Icons.arrow_downward),
+                              style: bStyle,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => _modDate("+", const Duration(hours: 1)),
+                              child: const Icon(Icons.arrow_upward),
+                              style: bStyle,
+                            ),
+                            Text(
+                              targetString.hour,
+                              style: tStyle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () => _modDate("-", const Duration(hours: 1)),
+                              child: const Icon(Icons.arrow_downward),
+                              style: bStyle,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () =>
+                                  _modDate("+", const Duration(minutes: 1)),
+                              child: const Icon(Icons.arrow_upward),
+                              style: bStyle,
+                            ),
+                            Text(
+                              targetString.minute,
+                              style: tStyle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () =>
+                                  _modDate("-", const Duration(minutes: 1)),
+                              child: const Icon(Icons.arrow_downward),
+                              style: bStyle,
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                    Text(
-                      DateFormat('dd').format(date),
-                      style: tStyle,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _modDay("-", const Duration(days: 1)),
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _modDay("+", const Duration(days: 99)),
-                      child: const Icon(Icons.arrow_upward),
-                    ),
-                    Text(
-                      DateFormat('MM').format(date),
-                      style: tStyle,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _modDay("-", const Duration(days: 99)),
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _modDay("+", const Duration(days: 365)),
-                      child: const Icon(Icons.arrow_upward),
-                    ),
-                    Text(
-                      DateFormat('yyyy').format(date),
-                      style: tStyle,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _modDay("-", const Duration(days: 365)),
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _modDay("+", const Duration(hours: 1)),
-                      child: const Icon(Icons.arrow_upward),
-                    ),
-                    Text(
-                      DateFormat('HH').format(date),
-                      style: tStyle,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _modDay("-", const Duration(hours: 1)),
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _modDay("+", const Duration(minutes: 1)),
-                      child: const Icon(Icons.arrow_upward),
-                    ),
-                    Text(
-                      DateFormat('mm').format(date),
-                      style: tStyle,
-                    ),
-                    ElevatedButton(
-                      onPressed: () => _modDay("-", const Duration(minutes: 1)),
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            ),
-            Visibility(
-                visible: inProgress,
-                child: Wrap(
-              spacing: 20,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      DateFormat('dd').format(date),
-                      style: cStyle,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      DateFormat('MM').format(date),
-                      style: cStyle,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      DateFormat('yyyy').format(date),
-                      style: cStyle,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      DateFormat('HH').format(date),
-                      style: cStyle,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      DateFormat('mm').format(date),
-                      style: cStyle,
-                    ),
-                  ],
-                )
-              ],
-            ),
-            ),
-          ],
-        )
+                  ),
+                  Visibility(
+                    visible: inProgress,
+                    child: Column(
+                      children:[
+                        Visibility(
+                            child:
+                            Text(
+                              travelText,
+                              style: tStyle,
+                            ),
+                        ),
+                        Wrap(
+                          spacing: 20,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  dateString.day,
+                                  style: cStyle,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  dateString.month,
+                                  style: cStyle,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  dateString.year,
+                                  style: cStyle,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  dateString.hour,
+                                  style: cStyle,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  dateString.minute,
+                                  style: cStyle,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ]
+                    )
+                  ),
+                ],
+              )),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _startCountdown,
         tooltip: 'Start',
         child: const Icon(Icons.play_arrow),
+        backgroundColor: Colors.black,
+        foregroundColor: defaultGreen,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
